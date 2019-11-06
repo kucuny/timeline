e5a35730ad157135c1a123d32699421b7301d167
Kookheon Kwon <kucuny@gmail.com>
2019-11-06 11:24:40 +0900

    Change auth urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('api/', include(('api.urls', 'api'), namespace='api')),
     path('admin/', admin.site.urls),
-    path('auth/api-auth/', include('rest_framework.urls')),
-    path('auth/social-auth/', include('social_django.urls', namespace='social')),
+    path('auth/api/', include('rest_framework.urls')),
+    path('auth/social/', include('social_django.urls', namespace='social')),
 ]
