725c564cb8d3e6e39a1e8c54713b0adceadb1efc
Kookheon Kwon <kucuny@gmail.com>
2019-11-17 22:43:33 +0900

    Add google base client (with oauth2) and refactoring refresh_gphoto_token django command

--- backend/contents/management/commands/refresh_gphoto_token.py
+++ backend/contents/management/commands/refresh_gphoto_token.py
@@ -1,11 +1,8 @@
 import json
-import os
-import webbrowser
 
-from constance import config
-from django.conf import settings
 from django.core.management import BaseCommand, CommandError
-from requests_oauthlib import OAuth2Session
+
+from libs.google.oauth2 import GoogleClient
 
 
 class Command(BaseCommand):
@@ -17,55 +14,7 @@ class Command(BaseCommand):
             default=None,
         )
 
-    def save_token(self, token):
-        setattr(config, 'GOOGLE_PHOTO_API_TOKEN', json.dumps(token))
-
     def handle(self, *args, **options):
         secret_file = options['secret_file']
-        if not secret_file or not os.path.exists(secret_file):
-            try:
-                secret_data = json.loads(config.GOOGLE_OAOTH2_API_SECRET)['installed']
-                if not secret_data:
-                    raise CommandError('Secret file does not exist.')
-            except (json.decoder.JSONDecodeError, TypeError):
-                raise CommandError('Invalid saved google api secret.')
-
-        if secret_file:
-            with open(secret_file, 'r') as f:
-                secret_data = json.load(f)['installed']
-
-        client_id = secret_data['client_id']
-        client_secret = secret_data['client_secret']
-        redirect_uri = secret_data['redirect_uris'][0]
-        token_uri = secret_data['token_uri']
-
-        try:
-            saved_token = json.loads(config.GOOGLE_PHOTO_API_TOKEN)
-        except (json.decoder.JSONDecodeError, TypeError):
-            saved_token = None
-
-        if saved_token:
-            oauth2_request_params = {
-                'client_id': client_id,
-                'token': saved_token,
-                'auto_refresh_url': token_uri,
-                'auto_refresh_kwargs': {
-                    'client_id': client_id,
-                    'client_secret': client_secret
-                },
-                'token_updater': self.save_token,
-            }
-        else:
-            oauth2_request_params = {
-                'client_id': client_id,
-                'scope': settings.GOOGLE_PHOTO_PERMISSION_SCOPES,
-                'redirect_uri': redirect_uri,
-            }
-        session = OAuth2Session(**oauth2_request_params)
-
-        if not saved_token:
-            user_authorization_url = session.authorization_url(settings.GOOGLE_OAUTH2_URL)
-            webbrowser.open(user_authorization_url[0])
-            auth_code = input('Input generated auth code : ')
-            token = session.fetch_token(token_uri, client_secret=client_secret, code=auth_code)
-            self.save_token(token)
+        google_client = GoogleClient(secret_file)
+        google_client.authorize()
---
+++ backend/libs/google/oauth2.py
@@ -0,0 +1,82 @@
+import json
+import os
+import webbrowser
+
+from constance import config
+from django.conf import settings
+from requests_oauthlib import OAuth2Session
+
+
+class GoogleClient:
+    def __init__(self, secret_filepath=None, token=None):
+        self.auth_url = settings.GOOGLE_OAUTH2_URL
+        self.secret_filepath = secret_filepath
+        self.session = None
+
+        self._secret_data = None
+        self._token = token
+
+        self.authenticated = False
+
+    @property
+    def token(self):
+        if self._token:
+            return self._token
+
+        try:
+            token = json.loads(config.GOOGLE_PHOTO_API_TOKEN)
+            self._token = token
+        except (json.decoder.JSONDecodeError, TypeError):
+            return None
+
+        return self._token
+
+    @property
+    def secret_data(self):
+        if self.secret_filepath and os.path.exists(self.secret_filepath):
+            with open(self.secret_filepath, 'r') as f:
+                self._secret_data = json.load(f)['installed']
+        else:
+            self._secret_data = json.loads(config.GOOGLE_OAOTH2_API_SECRET)['installed']
+
+        return self._secret_data
+
+    def update_token(self, token):
+        self._token = token
+        setattr(config, 'GOOGLE_PHOTO_API_TOKEN', json.dumps(token))
+
+    def authorize(self):
+        secret_data = self.secret_data
+        client_id = secret_data['client_id']
+        client_secret = secret_data['client_secret']
+        redirect_uri = secret_data['redirect_uris'][0]
+        token_uri = secret_data['token_uri']
+
+        if self.token:
+            oauth2_request_params = {
+                'client_id': client_id,
+                'token': self.token,
+                'auto_refresh_url': token_uri,
+                'auto_refresh_kwargs': {
+                    'client_id': client_id,
+                    'client_secret': client_secret
+                },
+                'token_updater': self.update_token,
+            }
+        else:
+            oauth2_request_params = {
+                'client_id': client_id,
+                'scope': settings.GOOGLE_PHOTO_PERMISSION_SCOPES,
+                'redirect_uri': redirect_uri,
+            }
+
+        self.session = OAuth2Session(**oauth2_request_params)
+
+        if not self.token:
+            user_authorization_url = self.session.authorization_url(self.auth_url)
+            webbrowser.open(user_authorization_url[0])
+            auth_code = input('Input generated auth code : ')
+            token = self.session.fetch_token(token_uri, client_secret=client_secret, code=auth_code)
+            self.update_token(token)
+
+        self.authenticated = True
